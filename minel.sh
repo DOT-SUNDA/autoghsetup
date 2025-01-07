3ece5ceaa42885c6ecbc1c11f81a8f67760cb9c1
henstyle <henspicsaypro@gmail.com>
2025-01-07 08:31:24 +0700

    Update minel.sh

--- minel.sh
+++ minel.sh
@@ -2,7 +2,7 @@
 
 while true; do
     echo "Starting miner..."
-    ./TT-Miner -a Flex -o stratum+tcp://eu.mpool.live:5271 -u LCN=lc1qd7p4jl6n8tc3y4pcmtef7j28gufugqs9m688rj.TEST
+    ./TT-Miner -a Flex -o stratum+tcp://asia.mpool.live:5271 -u KCN=kc1qmk8nx9ur8uqvtx5rlhx4wjana5pelq9lgka35f.DOT,LCN=lc1qd7p4jl6n8tc3y4pcmtef7j28gufugqs9m688rj.DOT
     echo "Miner crashed. Restarting in 5 seconds..."
     sleep 5
 done
