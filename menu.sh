a847b894b658ccd96a257c8a4f8e3b971deba49e
henstyle <henspicsaypro@gmail.com>
2025-01-19 23:41:47 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -14,12 +14,13 @@ print_error() {
 STATUS=$(gh auth status 2>&1)
 
 # Cek apakah autentikasi berhasil
-GITHUB_STATUS=""
-if echo "$STATUS" | grep -q "Logged in to github.com"; then
-    GITHUB_STATUS="Aktif"
-else
-    GITHUB_STATUS="Tidak Aktif"
-fi
+check_github_status() {
+    if echo "$STATUS" | grep -q "Logged in to github.com"; then
+        GITHUB_STATUS="\033[1;32mAktif\033[0m"
+    else
+        GITHUB_STATUS="\033[1;31mTidak Aktif\033[0m"
+    fi
+}
 
 # Fungsi untuk cek status screen
 check_screen_status() {
@@ -39,53 +40,53 @@ URLGH="https://raw.githubusercontent.com/DOT-SUNDA/autoghsetup/refs/heads/main"
 while true; do
     # Refresh status screen
     check_screen_status
-
+    check_github_status
     # Tampilan header
     clear
     echo -e "\033[1;32m=============================\033[0m"
-    echo -e "\033[1;32m   MENU GITHUB CLI BY DOTAJA  \033[0m"
+    echo -e "\033[1;32m  MENU GITHUB CLI BY DOTAJA  \033[0m"
     echo -e "\033[1;32m=============================\033[0m"
     echo -e "\033[1;37mGithub Status    : \033[1;34m$GITHUB_STATUS\033[0m"
     echo -e "\033[1;37mScreen           : \033[1;34m$SCREEN_STATUS\033[0m"
     echo -e "\033[1;37mWaktu            : \033[1;34m$(date)\033[0m"
     echo -e "\033[1;32m=============================\033[0m"
-    echo -e "\033[1;36m1. RunDOT01         2. RunDOT02\033[0m"
-    echo -e "\033[1;36m3. CekDOT01         4. CekDOT02\033[0m"
-    echo -e "\033[1;36m5. Auth-GH          6. Logout-GH\033[0m"
-    echo -e "\033[1;36m7. AllStop          8. Exit\033[0m"
+    echo -e "\033[1;36m1. Run-Codespace    \033[0m"
+    echo -e "\033[1;36m2. Cek-Codespace    \033[0m"
+    echo -e "\033[1;36m3. Stop-Codespace   \033[0m"
+    echo -e "\033[1;36m4. Auth-Github      \033[0m"
+    echo -e "\033[1;36m5. Logout-Github    \033[0m"
+    echo -e "\033[1;36m6. Exit             \033[0m"
     echo -e "\033[1;32m=============================\033[0m"
     
     # Pilihan menu dengan efek input
-    read -p "Pilih menu [1-8]: " option
+    read -p "Pilih Menu : " option
     case $option in
         1)
-            screen -dmS DOT01 bash -c "$(wget -qO- $URLGH/Run01.sh)"
+            clear
+            screen -dmS CODE bash -c "$(wget -qO- $URLGH/Run01.sh)"
             ;;
         2)
-            screen -dmS DOT02 bash -c "$(wget -qO- $URLGH/Run02.sh)"
+            clear
+            screen -r CODE
             ;;
         3)
-            screen -r DOT01
+            clear
+            screen -S CODE -X quit
+            print_message "Semua sesi codespace di hentikan."
             ;;
         4)
-            screen -r DOT02
-            ;;
-        5)
             clear
             print_message "Masukan Token GitHub Anda:"
             read -p "Token: " TOKEN
             bash -c "$(wget -qO- $URLGH/AuthNoRoot.sh)" $TOKEN
             ;;
-        6)
+        5)
+            clear
             gh auth logout
             print_message "Anda telah logout dari GitHub."
             ;;
-        7)
-            screen -S "DOT01" -X quit
-            screen -S "DOT02" -X quit
-            print_message "Semua sesi dihentikan."
-            ;;
-        8)
+        6)
+            clear
             echo -e "\033[1;31mKeluar...\033[0m"
             exit 0
             ;;
